a4cfda79ccd184daf7899e40815740ebe396e9da
prabal23 <prabal230193@gmail.com>
2022-09-25 17:50:47 +0600

    sample commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+  render() {
+    return (
+      <div>
+        skdfjklsf
+      </div>
+    )
+  }
+}
+
+export default index
